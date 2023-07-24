af6c5afa3898a73d03f2f3cbe09a5c92fa8a7d64
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-24 13:54:47 -0400

    Initial commit + header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+
+
+import headerImage from '../../assets/Stockholm Public Library.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>The Library at Your Doorstep</h1>
+        
+      </header>
+      <div className={classes['main-image']}>
+        <img src={headerImage} alt='Stockholm Public Library' />
+      </div>
+    </>
+  );
+};
+
+export default Header;

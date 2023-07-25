b26b511804b7db54945f51071f5301d448bfc75a
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-25 14:04:27 -0400

    Add total amount span to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -13,7 +13,8 @@ const Cart = (props) => {
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
-
+      <span>Total Amount</span>
+      <span>???</span>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>

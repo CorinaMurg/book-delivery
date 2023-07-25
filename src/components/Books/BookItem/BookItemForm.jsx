68e08e425eea9ae7d5f30a904889080687fccec7
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-25 14:19:12 -0400

    Add submit handler function

--- src/components/Books/BookItem/BookItemForm.jsx
+++ src/components/Books/BookItem/BookItemForm.jsx
@@ -1,7 +1,31 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './BookItemForm.module.css';
 
 const BookItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 3
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
+
   return (
     <form className={classes.form}>
        <Input
@@ -21,3 +45,10 @@ const BookItemForm = (props) => {
 };
 
 export default BookItemForm;
+
+
+
+
+
+
+

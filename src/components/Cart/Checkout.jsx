42fab8bee4036b28a59e1eb81fa8fec750ae4ae9
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-27 13:28:41 -0400

    Add checkout state to cart component

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -15,14 +15,10 @@ const Checkout = (props) => {
         <label htmlFor='street'>Street</label>
         <input type='text' id='street' />
       </div>
-      {/* <div className={classes.control}>
-        <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
-      </div> */}
-      {/* <div className={classes.control}>
-        <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
-      </div> */}
+      <div className={classes.control}>
+        <label htmlFor='phone'>Phone</label>
+        <input type='number' id='phone' />
+      </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.onCancel}>
           Cancel

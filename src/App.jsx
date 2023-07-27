d2b2a84f69dab47cf7c83ad06d88f8022ab70862
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-27 16:45:09 -0400

    Create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+
 import Books from './components/Books/Books';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
-import footerImage from '/public/assets/books on retro bike.jpg';
+
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -25,11 +27,8 @@ function App() {
           <Books />
         </div>
       </main>
-      <footer className="footer">
-        <div className="yellow">
-          <h4>There will be a beautiful paragraph here</h4>
-        </div>
-      </footer>
+      <Footer />
+        
     </CartProvider>
 
   );

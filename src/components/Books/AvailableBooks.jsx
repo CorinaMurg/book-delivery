0a39b5d8940d19c551412762b1ee9daf8cb774a4
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-27 11:42:15 -0400

    Write useEffect to fetch firebase data

--- src/components/Books/AvailableBooks.jsx
+++ src/components/Books/AvailableBooks.jsx
@@ -1,12 +1,35 @@
-
-
+import { useEffect, useState } from 'react';
 import BookItem from './BookItem/BookItem';
 import classes from './AvailableBooks.module.css';
-import booksData from './BooksData'
 
 const AvailableBooks = () => {
 
-  const booksList = booksData.map((book) => (
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://book-delivery-245da-default-rtdb.firebaseio.com/books.json');
+      const responseData = await response.json();
+
+      const loadedBooks = [];
+
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: key,
+          image: responseData[key].image,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          price: responseData[key].price,
+        });
+      }
+
+      setBooks(loadedBooks);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const booksList = books.map((book) => (
     <li key={book.id} className={classes.bookItem}>
       <BookItem
         id={book.id}

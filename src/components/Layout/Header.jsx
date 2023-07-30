90ddc3c598134fbbf515959a077f0c48ce21480c
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-30 18:00:10 -0400

    Fixed aria-hidden for store hours, working now

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -34,8 +34,12 @@ const Header = (props) => {
         </div>
         <div className={classes['hero--hours']}>
           <h2>Store Hours</h2>
-          <p aria-label="Monday to Friday: 7 AM to 9 PM">MON - FRI: 7AM - 9PM</p>
-          <p aria-label="Saturday to Sunday: 8 AM to 7 PM">SAT - SUN: 8AM - 7PM</p>
+          
+          <p className={classes['visually-hidden']}>Monday to Friday: 7 AM to 9 PM</p>
+          <p className={classes['visually-hidden']}>Saturday to Sunday: 8 AM to 7 PM</p>
+          <p aria-hidden={true}>MON - FRI: 7AM - 9PM</p>
+          <p aria-hidden={true}>SAT - SUN: 8AM - 7PM</p>
+          
           <div></div>
           <p className={classes['hero--hours--blurb']}>Browse our extensive collections and select your next riveting read.</p>
         </div>
